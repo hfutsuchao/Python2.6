f00305334359e7208789d3123b01aeda63abc483
suchao <hfutsuchao@gmail.com>
2019-03-18 15:39:05 +0800

    new

--- work_tools/sem_roi_ana/get_uvpv_data.py
+++ work_tools/sem_roi_ana/get_uvpv_data.py
@@ -2,8 +2,8 @@
 from sqlalchemy import create_engine
 import pandas as pd
 from sqlalchemy.orm import sessionmaker
-#date = [('2017-01-01','2017-01-02'),('2017-01-02','2017-01-03')]
-date = [('2017-01-01','2017-07-01'),('2017-07-01','2018-01-01'),('2018-01-01','2018-07-01'),('2018-07-01','2019-03-01')]
+date = [('2018-02-22','2018-03-22'),('2019-02-11','2019-03-11')]
+#date = [('2017-01-01','2017-07-01'),('2017-07-01','2018-01-01'),('2018-01-01','2018-07-01'),('2018-07-01','2019-03-01')]
 #date = [('2017-07-01','2017-07-15'),('2017-07-15','2017-08-01')]
 #date = [('2017-08-01','2017-08-15'),('2017-08-15','2017-09-01')]
 df = pd.DataFrame()
@@ -20,6 +20,6 @@ def get_data(sql):
 
 for d in date:
 	#sql = "select date,ctime,cityId,uid,keywordId,url,refer from sem_action_log where date>='" + d[0] + "' and date<'" + d[1] + "' order by date asc;"
-	sql = "select substr(date,1,10) dt,uid from sem_action_log where date>='" + d[0] + "' and date<'" + d[1] + "' and cityId in (12,13,16,17) and category in (1) and keywordId<>'';"
+	sql = "select substr(date,1,10) dt,uid from sem_action_log where date>='" + d[0] + "' and date<'" + d[1] + "' and cityId in (45) and category in (1) and keywordId<>'';"
 	df = pd.concat([df,get_data(sql)])
 df.to_csv('./uvlog.csv')
